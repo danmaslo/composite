5b6aa0611be346623319227b123739731ba169c8
Daniel Máslo <hi@danielmaslo.com>
2017-05-01 18:38:37 +0200

    object inits
    composite generator upgraded;

--- main.js
+++ main.js
@@ -2,39 +2,47 @@ const Vector = require('./lib/2d/Vector.js');
 const Circle = require('./lib/2d/Circle.js');
 const Composite = require('./lib/2d/Composite.js');
 
-const w = 100;
-const h = 100;
-const b = Composite.PERIODIC;
+var c = new Composite({
+	"width": 100,
+	"height": 100,
+	"bounds": Composite.NON_PERIODIC
+});
 
-var c = new Composite(w, h, b);
+c.generate({
+	"until": { // stop when reach first of something
+		"tries": 10000 // number of random particles
+	},
+	"method": Composite.PIC,
+	"particles": [
+		{
+			"radius": 1,
+			"probability": 0.5
+		},
+		{
+			"radius": 2,
+			"probability": 0.25
+		},
+		{
+			"radius": 3,
+			"probability": 0.25
+		}
+	]
+});
 
-for (var i = 0; i < 100000; i++) {
-	var x = w * Math.random();
-	var y = h * Math.random();
-	var r = 1.0;
-
-	c.add(new Circle(new Vector(x, y), r));
-}
+c.saveSvg("renders/sample");
 
 /*
-for (var i = 0; i < 1000; i++) {
-	var x = w * Math.random();
-	var y = h * Math.random();
-	var r = 10.0;
-
-	c.add(new Circle(new Vector(x, y), r));
-}
-
-for (var i = 0; i < 1000; i++) {
-	var x = w * Math.random();
-	var y = h * Math.random();
+for (var i = 0; i < 100000; i++) {
+	var x = 100 * Math.random();
+	var y = 100 * Math.random();
 	var r = 1.0;
 
 	c.add(new Circle(new Vector(x, y), r));
 }
-*/
 
 c.saveSvg("renders/100x100-periodic__1");
+*/
+
 
 /*
 // ideal code
