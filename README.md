7cd6ec18f79c6fd491c5f0e56c2966725ced7b07
Daniel Máslo <hi@danielmaslo.com>
2017-05-02 22:12:15 +0200

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # composite
+
+![N|Solid](http://www.danielmaslo.com/img/github.svg)
+
 easy computer physics tool for composite structures
--- main.js
+++ main.js
@@ -3,33 +3,37 @@ const Circle = require('./lib/2d/Circle.js');
 const Composite = require('./lib/2d/Composite.js');
 
 var c = new Composite({
-	"width": 100,
-	"height": 100,
+	"width": 888,
+	"height": 222,
 	"bounds": Composite.NON_PERIODIC
 });
 
 c.generate({
 	"until": { // stop when reach first of something
-		"tries": 10000 // number of random particles
+		"tries": 100000 // number of random particles
 	},
 	"method": Composite.PIC,
 	"particles": [
 		{
 			"radius": 1,
-			"probability": 0.5
+			"probability": 0.533333333
 		},
 		{
 			"radius": 2,
-			"probability": 0.25
+			"probability": 0.266666666
 		},
 		{
 			"radius": 3,
-			"probability": 0.25
+			"probability": 0.133333333
+		},
+		{
+			"radius": 4,
+			"probability": 0.066666666
 		}
 	]
 });
 
-c.saveSvg("renders/sample");
+c.saveSvg("renders/github");
 
 /*
 for (var i = 0; i < 100000; i++) {
