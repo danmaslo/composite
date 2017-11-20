187c7b34d05e26b78c17eca08037268a6d240238
Daniel Máslo <hi@danielmaslo.com>
2017-11-20 14:09:37 +0100

    save and load Composite to/from txt file

--- main.js
+++ main.js
@@ -3,34 +3,66 @@ const Circle = require('./lib/2d/Circle.js');
 const Composite = require('./lib/2d/Composite.js');
 
 var c = new Composite({
-	"width": 444,
-	"height": 444,
+	"width": 200,
+	"height": 200,
 	"bounds": Composite.NON_PERIODIC
 });
 
 c.generate({
 	"until": { // stop when reach first of something
-		"tries": 100000, // number of random particles
-		"time": 60 // number of seconds
+		"tries": 1000000, // number of random particles
+		"time": 300 // number of seconds
 	},
 	"method": Composite.PIC,
 	"particles": [
 		{
-			"radius": 1,
-			"probability": 1.0
+			"radius": 4.0,
+			"probability": 1
 		}
 	]
 });
+/*
+c.generate({
+	"until": { // stop when reach first of something
+		"tries": 10000000, // number of random particles
+		"time": 300 // number of seconds
+	},
+	"method": Composite.PIC,
+	"particles": [
+		{
+			"radius": 4.0,
+			"probability": 0.5
+		},
+		{
+			"radius": 2.0,
+			"probability": 0.25
+		},
+		{
+			"radius": 1.0,
+			"probability": 0.125
+		},
+		{
+			"radius": 0.5,
+			"probability": 0.0625
+		},
+		// tricky radius only for probability sum == 1
+		{
+			"radius": 1000,
+			"probability": 0.0625
+		}
+	]
+});
+*/
 
 c.export({
 	"type": Composite.EXPORT_SVG,
-	"file": "renders/sampleExport"
+	"file": "renders/preSAVE"
 });
 
-/*
-process.stdout.write("Downloading 1 bytes\r");
-process.stdout.write("Downloading 2 bytes\r");
-process.stdout.write("Downloading 3 bytes\r");
-process.stdout.write("Downloading 4 bytes\r");
-process.stdout.write("Downloading 5 bytes");
-*/
+c.saveTxt("saves/comp");
+c.loadTxt("saves/comp");
+
+c.export({
+	"type": Composite.EXPORT_SVG,
+	"file": "renders/postLOAD"
+});
